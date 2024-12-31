5fb0426b4b4a1ad0771f0843cc951137773dd93c
ahmed2402 <ahmedraza312682@gmail.com>
2025-01-01 01:37:45 +0500

    ex sol

---
+++ exercise/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <div className="navbar bg-blue-600 text-white flex ">
+        <ul className='flex gap-1'>
+            <li>Home</li>
+            <li>Page</li>
+            <li>About</li>
+        </ul>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
